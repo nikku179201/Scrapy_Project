31193a6b60c6d569886fb0d4080be225f6e0e414
Nitin <vinodlucky1981@gmail.com>
2023-01-28 19:05:40 +0530

    updated

--- indeed.jl
+++ indeed.jl
@@ -1,15 +1,15 @@
 {"job": "Python developers", "companyName": "iNFOX Technologies", "location": "Kerala"}
-{"job": "Python Developer", "companyName": "Infosys Limited", "location": "Hyderabad, Telangana"}
-{"job": "Software Engineer I", "companyName": "Mastercard", "location": "Pune, Maharashtra"}
+{"job": "Python Intern", "companyName": "Rabbit and Tortoise Technology Solutions", "location": "Pune, Maharashtra"}
 {"job": "Internships for Python / EmberJS Developers for eventyay.com", "companyName": "FOSSASIA", "location": "Remote"}
 {"job": "Python", "companyName": "Fleming Embedded and Software Solutions", "location": "Ernakulam, Kerala"}
-{"job": "Python Developer", "companyName": "vInnovate Technologies", "location": "Remote"}
-{"job": "Python Intern", "companyName": "Rabbit and Tortoise Technology Solutions", "location": "Pune, Maharashtra"}
-{"job": "Python Developer", "companyName": "Coditation Systems", "location": "Pune, Maharashtra"}
-{"job": "Python Developer", "companyName": "Talentica Software India", "location": "Pune, Maharashtra"}
-{"job": "Python Developer", "companyName": "PWS Information Technology", "location": "Lucknow, Uttar Pradesh"}
-{"job": "Python Developer", "companyName": "Incsub, LLC", "location": "+1 location"}
 {"job": "Python Developers", "companyName": "Apptware Solutions", "location": "Pune, Maharashtra"}
-{"job": "Python Developer", "companyName": "PST.AG", "location": "Remote"}
-{"job": "Python Developer", "companyName": "Infrrd", "location": "Remote"}
+{"job": "2 Junior Python Developers", "companyName": "CAL4CARE", "location": "India"}
+{"job": "Python Developer", "companyName": "PWS Information Technology", "location": "Lucknow, Uttar Pradesh"}
+{"job": "Python Developer", "companyName": "Talentica Software India", "location": "Pune, Maharashtra"}
+{"job": "Python Developer", "companyName": "Infosys Limited", "location": "Hyderabad, Telangana"}
+{"job": "Python Developer", "companyName": "Coditation Systems", "location": "Pune, Maharashtra"}
+{"job": "Python developer", "companyName": "snapblocs", "location": "Remote"}
+{"job": "Python Developer", "companyName": "vInnovate Technologies", "location": "Remote"}
+{"job": "Python Developers", "companyName": "Laddu Systems", "location": "Remote in Hyderabad, Telangana"}
 {"job": "Python Developer", "companyName": "BestPeers", "location": "Remote in Indore, Madhya Pradesh"}
+{"job": "Python Developer", "companyName": "Infrrd", "location": "Remote"}
